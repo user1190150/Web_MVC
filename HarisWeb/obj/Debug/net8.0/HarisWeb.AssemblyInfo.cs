b55e4b51916290f83ab891a0eabd17b10f94d48a
Besirevic Haris <besirevic205@gmail.com>
2024-08-07 20:19:21 +0200

    Added NuGet Packages: Microsoft.EntityFrameworkCore Microsoft.EntityFrameworkCore.SqlServer Microsoft.EntityFrameworkCore.Tools

--- HarisWeb/obj/Debug/net8.0/HarisWeb.AssemblyInfo.cs
+++ HarisWeb/obj/Debug/net8.0/HarisWeb.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HarisWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e506d64b60de5410af5977b6bfef2d0bf7a58d2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+478b0f769756adddd72a7c824473044216162678")]
 [assembly: System.Reflection.AssemblyProductAttribute("HarisWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HarisWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
