9f24555ca558962de2670a3afc7b01bbf412353a
Besirevic Haris <besirevic205@gmail.com>
2024-08-07 21:31:30 +0200

    Service in Program.cs configured Connection String

--- HarisWeb/Program.cs
+++ HarisWeb/Program.cs
@@ -1,10 +1,12 @@
 using HarisWeb.Data;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-builder.Services.AddDbContext<ApplicationDbContext>();
+builder.Services.AddDbContext<ApplicationDbContext>(options => 
+        options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
