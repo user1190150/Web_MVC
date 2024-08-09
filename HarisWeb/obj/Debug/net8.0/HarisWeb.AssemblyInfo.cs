02322199e20a757415bcaae25da4d869a26f0c49
Besirevic Haris <besirevic205@gmail.com>
2024-08-09 18:00:37 +0200

    added a view page for category

--- HarisWeb/obj/Debug/net8.0/HarisWeb.AssemblyInfo.cs
+++ HarisWeb/obj/Debug/net8.0/HarisWeb.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HarisWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+18179d93e2daf08c930f0bff26fdc06fabda6742")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ef5a1969077698fa5f06174ed1a7a0486e090fa")]
 [assembly: System.Reflection.AssemblyProductAttribute("HarisWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HarisWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
