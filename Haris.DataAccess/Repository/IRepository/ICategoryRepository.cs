3bbc97289ce78695e874693574003faa9ff2c3e3
Besirevic Haris <besirevic205@gmail.com>
2024-08-13 23:46:55 +0200

    Implement ICategoryRepository

---
+++ Haris.DataAccess/Repository/IRepository/ICategoryRepository.cs
@@ -0,0 +1,16 @@
+﻿using Haris.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Haris.DataAccess.Repository.IRepository
+{
+    internal interface ICategoryRepository : IRepository<Category>
+    {
+        void Update(Category obj);
+        void Save();
+        
+    }
+}
--- Haris.DataAccess/obj/Debug/net8.0/Haris.DataAccess.AssemblyInfo.cs
+++ Haris.DataAccess/obj/Debug/net8.0/Haris.DataAccess.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Haris.DataAccess")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e529e11bbf8f6bff67ca8e86783a09d0f2b13aba")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7b43eded175fe631357e8a4cc97edd8936c77c1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Haris.DataAccess")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Haris.DataAccess")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
