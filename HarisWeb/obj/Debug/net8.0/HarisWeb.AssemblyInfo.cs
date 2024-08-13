e529e11bbf8f6bff67ca8e86783a09d0f2b13aba
Besirevic Haris <besirevic205@gmail.com>
2024-08-13 21:25:31 +0200

    IRepository Interface

--- HarisWeb/obj/Debug/net8.0/HarisWeb.AssemblyInfo.cs
+++ HarisWeb/obj/Debug/net8.0/HarisWeb.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HarisWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe2c1e3222282924bfce2a89d9b8bf20363462e2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88f16703eba93f45736df35ea82968424ed3586a")]
 [assembly: System.Reflection.AssemblyProductAttribute("HarisWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HarisWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
