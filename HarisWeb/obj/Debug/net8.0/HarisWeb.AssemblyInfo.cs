f2812b839b3f1455ced388224202c3c23ac63ba5
Besirevic Haris <besirevic205@gmail.com>
2024-08-15 18:53:35 +0200

    adding Areas to Project

--- HarisWeb/obj/Debug/net8.0/HarisWeb.AssemblyInfo.cs
+++ HarisWeb/obj/Debug/net8.0/HarisWeb.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HarisWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+309017a572cd1cb8a31618e16090fcecee606880")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f91953ad4fb27b7805b8f4795b95625cb23810a")]
 [assembly: System.Reflection.AssemblyProductAttribute("HarisWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HarisWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
