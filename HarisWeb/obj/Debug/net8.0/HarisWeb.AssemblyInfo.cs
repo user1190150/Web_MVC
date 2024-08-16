c558edfb8e9f4cd076da1475f7c138a50400aa26
Besirevic Haris <besirevic205@gmail.com>
2024-08-16 22:53:18 +0200

    Edit and Create View modified with Textareas for Description

--- HarisWeb/obj/Debug/net8.0/HarisWeb.AssemblyInfo.cs
+++ HarisWeb/obj/Debug/net8.0/HarisWeb.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HarisWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+038be3259afde2ef99aa701b3b9107aa7012505d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd1b9ca4ea0986db789e4908954e35aaf021540f")]
 [assembly: System.Reflection.AssemblyProductAttribute("HarisWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HarisWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
