038be3259afde2ef99aa701b3b9107aa7012505d
Besirevic Haris <besirevic205@gmail.com>
2024-08-16 16:14:18 +0200

    added Product Repository Interface, Implementation and to UnitOfWork

---
+++ Haris.DataAccess/Repository/IRepository/IProductRepository.cs
@@ -0,0 +1,14 @@
+﻿using Haris.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Haris.DataAccess.Repository.IRepository
+{
+    public interface IProductRepository : IRepository<Product>
+    {
+        void Update(Product obj);
+    }
+}
--- Haris.DataAccess/obj/Debug/net8.0/Haris.DataAccess.AssemblyInfo.cs
+++ Haris.DataAccess/obj/Debug/net8.0/Haris.DataAccess.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Haris.DataAccess")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac2b33a84580387049d396ef4f271055787dc5f3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d1d57439b20ffc6fc5f3e3f43631d5cfb4e4303")]
 [assembly: System.Reflection.AssemblyProductAttribute("Haris.DataAccess")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Haris.DataAccess")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
