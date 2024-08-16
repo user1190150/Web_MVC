5d1d57439b20ffc6fc5f3e3f43631d5cfb4e4303
Besirevic Haris <besirevic205@gmail.com>
2024-08-16 15:53:55 +0200

    Add Migration Update DataBase Product

--- Haris.DataAccess/obj/Debug/net8.0/Haris.DataAccess.AssemblyInfo.cs
+++ Haris.DataAccess/obj/Debug/net8.0/Haris.DataAccess.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Haris.DataAccess")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f91953ad4fb27b7805b8f4795b95625cb23810a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac2b33a84580387049d396ef4f271055787dc5f3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Haris.DataAccess")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Haris.DataAccess")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- HarisWeb/obj/Debug/net8.0/HarisWeb.AssemblyInfo.cs
+++ HarisWeb/obj/Debug/net8.0/HarisWeb.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HarisWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df45b3cb67223180f5e35079c9c0ec182840c58e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac2b33a84580387049d396ef4f271055787dc5f3")]
 [assembly: System.Reflection.AssemblyProductAttribute("HarisWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HarisWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
