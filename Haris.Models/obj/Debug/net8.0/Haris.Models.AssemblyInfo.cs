7e0d541d87eace82e06655977b6a684279425849
Besirevic Haris <besirevic205@gmail.com>
2024-08-19 12:49:40 +0200

    implement text editor for description

--- Haris.Models/obj/Debug/net8.0/Haris.Models.AssemblyInfo.cs
+++ Haris.Models/obj/Debug/net8.0/Haris.Models.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Haris.Models")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd094866cbcc1f6e69c4a10d534171e0ee5b5035")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f0f91d01c4b0a3283e7c95027061430009c89784")]
 [assembly: System.Reflection.AssemblyProductAttribute("Haris.Models")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Haris.Models")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- HarisWeb/obj/Debug/net8.0/HarisWeb.AssemblyInfo.cs
+++ HarisWeb/obj/Debug/net8.0/HarisWeb.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HarisWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dfe2cec4d7c344e9fc65488c0a1c1f01064e9f9e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f0f91d01c4b0a3283e7c95027061430009c89784")]
 [assembly: System.Reflection.AssemblyProductAttribute("HarisWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HarisWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
