87ed30f9059f0836efc4dc73e9115f2899bfd45a
Besirevic Haris <besirevic205@gmail.com>
2024-08-20 15:06:14 +0200

    Action Method Details; Details View; Routing;

--- HarisWeb/Areas/Customer/Controllers/HomeController.cs
+++ HarisWeb/Areas/Customer/Controllers/HomeController.cs
@@ -23,6 +23,13 @@ namespace HarisWeb.Areas.Customer.Controllers
             return View(productList);
         }
 
+        public IActionResult Details(int productId)
+        {
+            Product product = _unitOfWork.Product.Get(u => u.Id == productId, includeProperties:"Category");
+
+            return View(product);
+        }
+
         public IActionResult Privacy()
         {
             return View();
--- HarisWeb/obj/Debug/net8.0/HarisWeb.AssemblyInfo.cs
+++ HarisWeb/obj/Debug/net8.0/HarisWeb.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HarisWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3d49cfab056ad695495413ae86dca639b6aef7f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4215825aeca544983a3c1d2c029f74ca86d986c7")]
 [assembly: System.Reflection.AssemblyProductAttribute("HarisWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HarisWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
