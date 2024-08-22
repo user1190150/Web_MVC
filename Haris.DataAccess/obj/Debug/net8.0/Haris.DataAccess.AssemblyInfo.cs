542b037951581ba512972c1314d1e605cd1ca688
Besirevic Haris <besirevic205@gmail.com>
2024-08-22 20:05:45 +0200

    migration added for IdentityTables in Database; Identity Tables added to DB

--- Haris.DataAccess/obj/Debug/net8.0/Haris.DataAccess.AssemblyInfo.cs
+++ Haris.DataAccess/obj/Debug/net8.0/Haris.DataAccess.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Haris.DataAccess")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3718b5ef634b0717d72a2107b8768077a1b20ccf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9466f587bb9e63494f9718fc685b0c21d019b396")]
 [assembly: System.Reflection.AssemblyProductAttribute("Haris.DataAccess")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Haris.DataAccess")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- HarisWeb/obj/Debug/net8.0/HarisWeb.AssemblyInfo.cs
+++ HarisWeb/obj/Debug/net8.0/HarisWeb.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HarisWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1057742d1b75919b48a5b1bc0915e36d9c9006da")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9466f587bb9e63494f9718fc685b0c21d019b396")]
 [assembly: System.Reflection.AssemblyProductAttribute("HarisWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HarisWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
