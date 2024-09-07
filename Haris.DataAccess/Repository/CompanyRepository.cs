29979a09e71276f6e6f16173de2e982249a89cfe
@haris <besirevic205@gmail.com>
2024-09-07 20:53:01 +0200

    added company model, repository and interfaces; not finished yet Controllergit add .

---
+++ Haris.DataAccess/Repository/CompanyRepository.cs
@@ -0,0 +1,25 @@
+﻿using Haris.DataAccess.Data;
+using Haris.DataAccess.Repository.IRepository;
+using Haris.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Haris.DataAccess.Repository
+{
+    internal class CompanyRepository : Repository<Company>, ICompanyRepository
+    {
+        private ApplicationDbContext _db;
+        public CompanyRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Update(Company obj)
+        {
+            _db.Companies.Update(obj);
+        }
+    }
+}
---
+++ HarisWeb/Areas/Admin/Controllers/CompanyController.cs
@@ -0,0 +1,29 @@
+﻿using Haris.DataAccess.Repository.IRepository;
+using Haris.Models;
+using Haris.Utility;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HarisWeb.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = SD.Role_Company)]
+    public class CompanyController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+        public CompanyController(IUnitOfWork unitOfWork, IWebHostEnvironment webHostEnvironment)
+        {
+            _unitOfWork = unitOfWork;
+            _webHostEnvironment = webHostEnvironment;
+        }
+
+        public IActionResult Index()
+        {
+            List<Company> objCompanyList = _unitOfWork.Company.GetAll().ToList();
+            return View(objCompanyList);
+        }
+
+    }
+}
