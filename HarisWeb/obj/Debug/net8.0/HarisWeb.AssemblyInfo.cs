11e0e64325ab28b286ab2a4a3cf37233355ad8ec
@haris <besirevic205@gmail.com>
2024-10-14 19:47:31 +0200

    Shopping Cart UI added and Controller added

--- HarisWeb/obj/Debug/net8.0/HarisWeb.AssemblyInfo.cs
+++ HarisWeb/obj/Debug/net8.0/HarisWeb.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HarisWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a92bf6deadee29f432c8b53852f450f1d73cd76")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eb8a3a03c8d4ea1e8ecb124aa72e8cce7f908e98")]
 [assembly: System.Reflection.AssemblyProductAttribute("HarisWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HarisWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
