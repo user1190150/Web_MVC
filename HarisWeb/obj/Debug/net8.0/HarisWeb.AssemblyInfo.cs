51531c290bc9046041a513a5a5d3271db477b314
@haris <besirevic205@gmail.com>
2024-10-21 12:24:56 +0200

    Summary UI implemented; View added ViewModel;

--- HarisWeb/obj/Debug/net8.0/HarisWeb.AssemblyInfo.cs
+++ HarisWeb/obj/Debug/net8.0/HarisWeb.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HarisWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f7bdc372246958d695f103d6829c6669774ecdb6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af9099f7bafdc48d857bf5ef8eb6fc25985131fd")]
 [assembly: System.Reflection.AssemblyProductAttribute("HarisWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HarisWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
