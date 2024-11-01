6c480a2f219a85c5564bee3129e71bf5c520773e
@haris <besirevic205@gmail.com>
2024-11-01 20:37:57 +0100

    display order details in order management

--- HarisWeb/obj/Debug/net8.0/HarisWeb.AssemblyInfo.cs
+++ HarisWeb/obj/Debug/net8.0/HarisWeb.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HarisWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a860c4dc4a5c3c2f3f6a483f0c113a75052c6a20")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bdf652f42d4547853859a9d1b8d0ab07e5959291")]
 [assembly: System.Reflection.AssemblyProductAttribute("HarisWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HarisWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
